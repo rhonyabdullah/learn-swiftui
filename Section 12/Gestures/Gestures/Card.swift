03657bb62f13cf0fd2727f9e33148a6375b293b7
Rhony <rsiagian@quadrant.io>
2022-12-22 11:07:54 +0700

    72. Magnification gestures Zoom In Out

--- Section 12/Gestures/Gestures/Card.swift
+++ Section 12/Gestures/Gestures/Card.swift
@@ -4,21 +4,36 @@
 //
 //  Created by Rhony Abdullah Siagian on 21/12/22.
 //
-
+import Foundation
 import SwiftUI
 
 struct Card: View {
 
     let tapped: Bool
+    @State private var scale: CGFloat = 1
 
     var body: some View {
 
         VStack {
+
+            Image("Cappuccino")
+                .resizable()
+                .frame(width: 300, height: 300)
+                .cornerRadius(8)
+                .scaleEffect(scale)
+                .gesture(
+                    MagnificationGesture()
+                        .onChanged { value in
+                            scale = value.magnitude
+                        }
+                )
+
             Text("Card")
                 .font(.largeTitle)
                 .foregroundColor(Color.white)
+
         }.frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(tapped ? Color.orange : Color.purple)
+            .background(tapped ? Color.orange : Color.indigo)
             .cornerRadius(30)
 
     }
