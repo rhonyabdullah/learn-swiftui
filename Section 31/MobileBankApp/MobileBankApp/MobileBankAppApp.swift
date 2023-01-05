71cde85ae316fe0d6af8eece3a8c4682785785b8
Rhony <rsiagian@quadrant.io>
2023-01-05 13:09:07 +0700

    180. Mobile Banking App

---
+++ Section 31/MobileBankApp/MobileBankApp/MobileBankAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileBankAppApp.swift
+//  MobileBankApp
+//
+//  Created by Mohammad Azam on 10/7/20.
+//
+
+import SwiftUI
+
+@main
+struct MobileBankAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
