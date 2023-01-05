12f1997287e6ad5d8ea3e408627580c095839238
Rhony <rsiagian@quadrant.io>
2023-01-05 13:27:59 +0700

    181. Plants App

---
+++ Section 31/PlantsApp/PlantsApp/PlantsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantsAppApp.swift
+//  PlantsApp
+//
+//  Created by Mohammad Azam on 9/28/20.
+//
+
+import SwiftUI
+
+@main
+struct PlantsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
